16e05e2dd26f9051ea73939d21f2ebd590e94b84
sydneyrichman <79024831+sydneyrichman@users.noreply.github.com>
2024-01-28 21:55:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function updateCounter() {
       else if(index % 80 === 2 || index % 80 === 7 || index % 80 === 10 || index % 80 === 14 || index % 80 === 18 || index % 80 === 23 || index % 80 === 25 || index % 80 === 30 || index % 80 === 34 || index % 80 === 37 || index % 80 === 42 || index % 80 === 47 || index % 80 === 51 || index % 80 === 55 || index % 80 === 58 || index % 80 === 62 || index % 80 === 66 || index % 80 === 69 || index % 80 === 75 || index % 80 === 78){
         styleTwo++;
       }
-      else if(index % 80 === 3 || index % 80 === 6 || index % 80 === 11 || index % 80 === 15 || index % 80 === 19 || index % 80 === 22 || index % 80 === 27 || index % 80 === 29 || index % 80 === 35 || index % 80 === 38 || index % 80 === 43 || index % 80 === 46 || index % 80 === 49 || index % 80 === 56 || index % 80 === 59 || index % 80 === 64 || index % 80 === 67 || index % 80 === 7 || index % 80 === 76 || index % 80 === 0){
+      else if(index % 80 === 3 || index % 80 === 6 || index % 80 === 11 || index % 80 === 15 || index % 80 === 19 || index % 80 === 22 || index % 80 === 27 || index % 80 === 29 || index % 80 === 35 || index % 80 === 38 || index % 80 === 43 || index % 80 === 46 || index % 80 === 49 || index % 80 === 56 || index % 80 === 59 || index % 80 === 64 || index % 80 === 67 || index % 80 === 71 || index % 80 === 76 || index % 80 === 0){
         styleThree++;
       }
       else {
